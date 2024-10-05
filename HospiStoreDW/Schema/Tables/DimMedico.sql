e403d67fc109f9c7eb1377cb36b365c021510e8d
“Harold <harold.romero.albarado@gmail.com>
2024-10-05 12:37:45 -0400

    schema bisita cubo

---
+++ HospiStoreDW/Schema/Tables/DimMedico.sql
@@ -0,0 +1,7 @@
+﻿CREATE TABLE Dim_Medico (
+    medico_sk INT IDENTITY(1,1) PRIMARY KEY,  
+    medico_id INT,
+    nombre VARCHAR(255),                      
+    apellido VARCHAR(255),                    
+    fecha_de_nacimiento DATE                 
+);
