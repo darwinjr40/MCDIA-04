b41d731e64a3018ec0fae5a0670681c4746a6cd8
darwin mamani <darwin_xd10@hotmail.com>
2024-10-05 13:52:47 -0400

    feat: clean code

--- proyecto-maestria/Databases/SanitaryOLTP/Schema/Tables/SERVICIO.sql
+++
@@ -1,7 +0,0 @@
-﻿CREATE TABLE [dbo].[SERVICIO] (
-    [id]         INT           NOT NULL,
-    [nombre]     VARCHAR (100) NOT NULL,
-    [comentario] VARCHAR (255) NOT NULL,
-    CONSTRAINT [PK_SERVICIO] PRIMARY KEY CLUSTERED ([id] ASC)
-);
-
