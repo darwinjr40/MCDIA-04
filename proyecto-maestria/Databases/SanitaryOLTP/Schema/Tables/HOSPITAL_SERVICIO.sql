2fd63bd1f0c68af6bab3494f03221b90fc6fdca0
darwin mamani <darwin_xd10@hotmail.com>
2024-10-05 13:41:53 -0400

    delete -xd

---
+++ proyecto-maestria/Databases/SanitaryOLTP/Schema/Tables/HOSPITAL_SERVICIO.sql
@@ -0,0 +1,11 @@
+﻿CREATE TABLE [dbo].[HOSPITAL_SERVICIO] (
+    [id]                 INT NOT NULL,
+    [total_camas]        INT NOT NULL,
+    [total_camas_libres] INT NOT NULL,
+    [servicio_id]        INT NOT NULL,
+    [hospital_id]        INT NOT NULL,
+    CONSTRAINT [PK_HOSPITAL_SERVICIO] PRIMARY KEY CLUSTERED ([id] ASC),
+    CONSTRAINT [FK_HOSPITAL_SERVICIO_HOSPITAL] FOREIGN KEY ([hospital_id]) REFERENCES [dbo].[HOSPITAL] ([id]),
+    CONSTRAINT [FK_HOSPITAL_SERVICIO_SERVICIO] FOREIGN KEY ([servicio_id]) REFERENCES [dbo].[SERVICIO] ([id])
+);
+
---
+++ proyecto-maestria/Databases/SanitaryOLTP/Scripts/Script.PostDeployment.sql
@@ -0,0 +1,47 @@
+﻿/*
+Plantilla de script posterior a la implementación							
+--------------------------------------------------------------------------------------
+ Este archivo contiene instrucciones de SQL que se anexarán al script de compilación.		
+ Use la sintaxis de SQLCMD para incluir un archivo en el script posterior a la implementación.			
+ Ejemplo:      :r .\miArchivo.sql								
+ Use la sintaxis de SQLCMD para hacer referencia a una variable en el script posterior a la implementación.		
+ Ejemplo:      :setvar TableName miTabla							
+               SELECT * FROM [$(TableName)]					
+--------------------------------------------------------------------------------------
+*/
+GO
+delete from visita;
+GO
+delete from MEDICO_HOSPITAL_SERVICIO;
+GO
+delete from HOSPITAL_SERVICIO;
+GO
+delete from servicio;
+GO
+delete from hospital;
+GO
+delete from medico;
+GO
+--DBCC CHECKIDENT ('medico', RESEED);
+GO
+delete from HISTORIAL_CLINICO;
+GO
+delete from paciente;
+GO
+
+
+
+
+
+
+:r .\paciente.data.sql
+:r .\historial_clinico.data.sql
+:r .\StoredProcedures\medico.data.sql
+GO
+EXEC pro_addMedicos 50;
+GO
+:r .\hospital.data.sql
+:r .\servicio.data.sql
+:r .\hospital_servicio.data.sql
+:r .\medico_hospital_servicio.data.sql
+:r .\visita.data.sql
