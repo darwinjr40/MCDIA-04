5fb57036fba48e1b0e2c1afb1b298921c8596585
darwin mamani <darwin_xd10@hotmail.com>
2024-10-05 13:24:34 -0400

    refactor: fact-viita

--- HospiStoreDW/Schema/Tables/DimMedico.sql
+++ HospiStoreDW/Schema/Tables/DimMedico.sql
@@ -1,4 +1,4 @@
-﻿CREATE TABLE Dim_Medico (
+﻿CREATE TABLE DimMedico (
     medico_sk INT IDENTITY(1,1) PRIMARY KEY,  
     medico_id INT,
     nombre VARCHAR(255),                      
--- HospiStoreDW/Schema/Tables/FactVisita.sql
+++ HospiStoreDW/Schema/Tables/FactVisita.sql
@@ -1,20 +1,20 @@
-﻿CREATE TABLE Fact_Visita (
-    visita_sk INT IDENTITY(1,1) PRIMARY KEY,    
-    visita_id INT,
-    servicio_sk INT,                            
-    medico_sk INT,                               
-    hospital_sk INT,                            
-    paciente_sk INT,                             
-    historial_clinico_sk INT,                   
-    fecha_sk INT,                                
-    numero_cama INT,                             
-    cantidad INT,                                
-    monto_total DECIMAL(10, 2),                  
+﻿create table fact_visita (
+    visita_sk int identity(1,1) primary key,    
+    visita_id int,
+    servicio_sk int,                            
+    medico_sk int,                               
+    hospital_sk int,                            
+    paciente_sk int,                             
+    historial_clinico_sk int,                   
+    fecha_sk int,                                
+    numero_cama int,                             
+    cantidad int,                                
+    monto_total decimal(10, 2),                  
 
-    CONSTRAINT FK_Fact_Visita_Servicio FOREIGN KEY (servicio_sk) REFERENCES DimServicios(servicio_sk),
-    CONSTRAINT FK_Fact_Visita_Medico FOREIGN KEY (medico_sk) REFERENCES DimMedico(medico_sk),
-    CONSTRAINT FK_Fact_Visita_Hospital FOREIGN KEY (hospital_sk) REFERENCES DimHospital(hospital_sk),
-    CONSTRAINT FK_Fact_Visita_Paciente FOREIGN KEY (paciente_sk) REFERENCES DimHistorialClinico(paciente_sk),
-    CONSTRAINT FK_Fact_Visita_Historial FOREIGN KEY (historial_clinico_sk) REFERENCES DimHistorialClinico(historial_clinico_sk),
-    CONSTRAINT FK_Fact_Visita_Fecha FOREIGN KEY (fecha_sk) REFERENCES DimFecha(fecha_sk)
+    constraint fk_fact_visita_servicio foreign key (servicio_sk) references dimservicios(servicios_sk),
+    constraint fk_fact_visita_medico foreign key (medico_sk) references dimmedico(medico_sk),
+    constraint fk_fact_visita_hospital foreign key (hospital_sk) references dimhospital(hospital_sk),
+    --constraint fk_fact_visita_paciente foreign key (paciente_sk) references dimhistorialclinico(paciente_sk),
+    --constraint fk_fact_visita_historial foreign key (historial_clinico_sk) references dimhistorialclinico(historial_clinico_sk),
+    --constraint fk_fact_visita_fecha foreign key (fecha_sk) references dimfecha(fecha_sk)
 );
