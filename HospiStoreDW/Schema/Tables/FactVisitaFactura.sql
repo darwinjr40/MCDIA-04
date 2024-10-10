9d1f352074e9f2e02ac5554116d69cca27bf5243
darwin mamani <darwin_xd10@hotmail.com>
2024-10-10 03:27:41 -0400

    feat: add procedimiento almacenado

--- HospiStoreDW/Schema/Tables/FactVisitaFactura.sql
+++ HospiStoreDW/Schema/Tables/FactVisitaFactura.sql
@@ -11,10 +11,10 @@
     nro_habitacion INT,
     fecha_ingreso DATE,
     fecha_salida DATE,
-    fecha_sk int,
+    fecha_id int,
     FOREIGN KEY (hospital_servicio_sk) REFERENCES dim_hospital_Servicio(hospital_servicio_sk),
     FOREIGN KEY (medico_sk) REFERENCES dim_medico(medico_sk),
     FOREIGN KEY (paciente_historial_sk) REFERENCES dim_paciente_historial(paciente_historial_sk),
     FOREIGN KEY (examen_sk) REFERENCES dim_examen(examen_sk),
-    FOREIGN KEY (fecha_sk) REFERENCES dim_fecha(fecha_id),    
+    FOREIGN KEY (fecha_id) REFERENCES dim_fecha(fecha_id),    
 );
