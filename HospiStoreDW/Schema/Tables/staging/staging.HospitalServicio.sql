414a7405513de5a986474b8949471a3312adfa42
darwin mamani <darwin_xd10@hotmail.com>
2024-10-11 18:31:38 -0400

    feat: add new tables staging

---
+++ HospiStoreDW/Schema/Tables/staging/staging.HospitalServicio.sql
@@ -0,0 +1,9 @@
+﻿CREATE TABLE hospital_servicio (
+    hospital_servicio_sk INT,          
+    nombre_hospital VARCHAR(255), 
+    ciudad_hospital VARCHAR(100), 
+    nombre_servicio VARCHAR(100), 
+    total_camas INT,
+    total_camas_libres INT,
+    precio DECIMAL(10,2),    
+);
---
+++ HospiStoreDW/Schema/Tables/staging/staging.examen.sql
@@ -0,0 +1,6 @@
+﻿CREATE TABLE [dbo].[examen]
+(
+    [examen_sk] INT NOT NULL,         
+    [nombre] VARCHAR(100),
+    [descripcion] VARCHAR(255),    
+);
