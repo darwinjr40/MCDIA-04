f36c15db3230a88b841c5cd373854836f36b7c99
darwin mamani <darwin_xd10@hotmail.com>
2024-10-11 18:31:38 -0400

    feat: add new tables staging

---
+++ HospiStoreDW/Schema/Tables/staging/staging.VisitaFactura.sql
@@ -0,0 +1,14 @@
+﻿CREATE TABLE visita_factura (            
+    hospital_servicio_sk INT,
+    medico_sk INT, 
+    paciente_historial_sk INT, 
+    examen_sk INT,
+    total_factura DECIMAL(10,2),
+    cantidad_examenes INT,
+    monto_visita DECIMAL(10,2),
+    monto_examen DECIMAL(10,2),
+    nro_habitacion INT,
+    fecha_ingreso DATE,
+    fecha_salida DATE,
+    fecha_id int,    
+);
---
+++ HospiStoreDW/Schema/Tables/staging/staging.medico.sql
@@ -0,0 +1,7 @@
+﻿CREATE TABLE medico (
+    medico_sk INT,     
+    dni VARCHAR(20),
+    nombre VARCHAR(100),
+    apellido VARCHAR(100),
+    fecha_nacimiento DATE,     
+);
