c9ded318b63a9aa8dc0bc66bb73508b60c2f8fad
darwin mamani <darwin_xd10@hotmail.com>
2024-10-12 16:13:13 -0400

    feat: add method GetVisitaFacturaChangesByRowVersion

---
+++ DataBases/Schema/Programmability/Stored Procedures/dbo.GetVisitaFacturaChangesByRowVersion.sql
@@ -0,0 +1,29 @@
+﻿	CREATE PROCEDURE [dbo].[GetVisitaFacturaChangesByRowVersion]
+(
+   @startRow BIGINT,
+   @endRow  BIGINT
+)
+AS
+BEGIN
+    -- Selección de cambios en VISITA y sus facturas relacionadas
+    SELECT v.[id] AS visita_id,
+           v.[fecha],
+           v.[diagnostico],
+           v.[tratamiento],
+           v.[fecha_ingreso],
+           v.[fecha_salida],
+           v.[nro_habitacion],
+           f.[id] AS factura_id,
+           f.[fecha_emision],
+           f.[total],
+           f.[cant_examenes],
+           f.[monto_visita],
+           f.[monto_examen]
+    FROM [dbo].[visita] v
+    INNER JOIN [dbo].[factura] f ON v.[id] = f.[visita_id]
+    WHERE (v.[rowversion] > CONVERT(ROWVERSION, @startRow)
+           AND v.[rowversion] <= CONVERT(ROWVERSION, @endRow))
+      OR (f.[rowversion] > CONVERT(ROWVERSION, @startRow)
+           AND f.[rowversion] <= CONVERT(ROWVERSION, @endRow));
+END
+GO
