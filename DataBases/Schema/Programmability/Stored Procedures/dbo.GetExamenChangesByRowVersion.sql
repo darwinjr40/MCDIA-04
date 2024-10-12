ded81d6568c3bf36425711a2d84c630e86dd88ca
darwin mamani <darwin_xd10@hotmail.com>
2024-10-12 09:09:48 -0400

    feat: new etl

---
+++ DataBases/Schema/Programmability/Stored Procedures/dbo.GetExamenChangesByRowVersion.sql
@@ -0,0 +1,12 @@
+﻿CREATE PROCEDURE [dbo].[GetExamenChangesByRowVersion]
+(
+   @startRow BIGINT 
+   ,@endRow  BIGINT 
+)
+AS
+BEGIN
+	SELECT id, nombre, descripcion
+	FROM examen
+	--WHERE [rowversion] > CONVERT(ROWVERSION,@startRow) 
+	--AND [rowversion] <= CONVERT(ROWVERSION,@endRow)
+END
