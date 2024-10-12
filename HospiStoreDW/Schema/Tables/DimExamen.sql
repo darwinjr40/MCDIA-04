22574ee4ef51934387a655950637dcfdb816edac
“Harold <harold.romero.albarado@gmail.com>
2024-10-12 17:22:18 -0400

    task flow fact

--- HospiStoreDW/Schema/Tables/DimExamen.sql
+++ HospiStoreDW/Schema/Tables/DimExamen.sql
@@ -1,5 +1,6 @@
 ﻿CREATE TABLE dim_examen (
     examen_sk INT PRIMARY KEY IDENTITY(1,1),
+    hosptal_servicio_examen_id INT NOT NULL, 
     examen_id INT NOT NULL,     
     nombre VARCHAR(100),
     descripcion VARCHAR(255),
